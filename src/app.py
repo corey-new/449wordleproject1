e38c99d7ccbf1ac273aa60584b5112078791e1ea
bluepower9 <jarrod.leong@yahoo.com>
2022-10-17 18:24:02 -0700

    added main app.py file

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from venv import create
+from quart_schema import QuartSchema
+from quart import Quart, g, request
+from api.game.create import app_create
+
+
+if __name__ == '__main__':
+    app = Quart(__name__)
+    QuartSchema(app)
+
+
+    app.register_blueprint(app_create)
+
+    #config app here
+    pass
+
+    app.run(debug=True)
